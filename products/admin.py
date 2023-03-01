902eaab01ff25f0604ffa1b10cddf7374e77be40
erlixbaxman <beginerdd@gmail.com>
2023-03-02 00:55:01 +0300

    first commit

---
+++ products/admin.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from products.models import ProductCategory, Product, Basket
+
+
+admin.site.register(ProductCategory)
+admin.site.register(Basket)
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'quantity', 'category')
+    fields = ('name', 'image', 'description', 'short_description', ('price', 'quantity'), 'category')
+    readonly_fields = ('short_description',)
+    search_fields = ('name',)
+
---
+++ products/views.py
@@ -0,0 +1,46 @@
+from django.shortcuts import render, HttpResponseRedirect
+from products.models import ProductCategory, Product, Basket
+from django.contrib.auth.decorators import login_required
+from django.core.paginator import Paginator
+
+
+def index(request):
+    context = {'title': 'Store'}
+    return render(request, 'products/index.html', context)
+
+
+def products(request, category_id=None, page=1):
+    context = {'title': 'Store - Каталог', 'categories': ProductCategory.objects.all()}
+    if category_id:
+        products = Product.objects.filter(category_id=category_id)
+    else:
+        products = Product.objects.all()
+    paginator = Paginator(products, 3)
+    products_paginator = paginator.page(page)
+    context.update({'products': products_paginator})
+    return render(request, 'products/products.html', context)
+
+
+@login_required(login_url='/users/login')
+def basket_add(request, product_id):
+    current_page = request.META.get('HTTP_REFERER')
+    product = Product.objects.get(id=product_id)
+    baskets = Basket.objects.filter(user=request.user, product=product)
+
+    if not baskets.exists():
+        Basket.objects.create(user=request.user, product=product, quantity=1)
+        return HttpResponseRedirect(current_page)
+    else:
+        basket = baskets.first()
+        basket.quantity += 1
+        basket.save()
+        return HttpResponseRedirect(current_page)
+
+
+@login_required(login_url='/users/login')
+def basket_delete(request, id):
+    basket = Basket.objects.get(id=id)
+    basket.delete()
+    return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
+
+
